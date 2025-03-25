ab6f224bba047be3137ba434061af8ee8d794067
nathanevaans <70093608+nathanevaans@users.noreply.github.com>
2025-03-25 21:00:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ col1, col2, col3 = st.columns(3)
 with col1:
     theta = st.slider("$\\theta$", min_value=0.1, max_value=3.0, value=1.0, step=0.1)
 with col2:
-    mu = st.slider("$\mu$", min_value=-5.0, max_value=5.0, value=0.0, step=0.1)
+    mu = st.slider("$\\mu$", min_value=-5.0, max_value=5.0, value=0.0, step=0.1)
 with col3:
-    sigma = st.slider("$\sigma$", min_value=0.1, max_value=2.0, value=0.5, step=0.1)
+    sigma = st.slider("$\\sigma$", min_value=0.1, max_value=2.0, value=0.5, step=0.1)
 
 plot_data = plot(theta, mu, sigma)
 # st.line_chart(plot_data)
